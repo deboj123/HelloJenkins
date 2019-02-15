ca8ca03a2a6786821fd3e85d9d85f483e0dfb1fc
deboj123 <debojyotipaul101@gmail.com>
2019-02-15 12:40:24 +0530

    Adding New Files

---
+++ HelloJenkins.java
@@ -0,0 +1,10 @@
+package com.debojyoti.jenkins.training;
+
+public class HelloJenkins {
+
+	public static void main(String[] args) {
+		System.out.println("This is first Jenkins Project.");
+
+	}
+
+}
